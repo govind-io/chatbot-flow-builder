484c0dfa352b5128082131376f5cbac2c743b22b
govind <gobind@loktantram.com>
2024-05-16 23:44:09 +0530

    feat: update styles and add grid design selector

--- src/components/home/contentArea/ContentArea.tsx
+++ src/components/home/contentArea/ContentArea.tsx
@@ -1,12 +1,19 @@
 import styles from "@styles/home/home-page.module.scss";
-import {memo, useContext, useMemo, useRef} from "react";
+import {memo, useCallback, useContext, useMemo, useRef, useState} from "react";
 import {Store} from "@store/context";
-import ReactFlow from "reactflow";
+import ReactFlow, {
+  Background,
+  BackgroundVariant,
+  MiniMap,
+  Node,
+  Panel,
+} from "reactflow";
 import "reactflow/dist/style.css";
 import MessageNodeTile from "./sidebar/nodes/MessageNodeTile";
 import useDropEnhanced from "@/hooks/useDropEnhanced";
 
 import useFlowEvents from "@/hooks/useFlowEvents";
+import {NodeTypes} from "@constants/nodeTypes";
 
 function ContentArea() {
   const ref = useRef<HTMLDivElement>(null);
@@ -21,10 +28,22 @@ function ContentArea() {
 
   const drop = useDropEnhanced();
 
+  const [backgroundVariant, setBackgroundVariant] = useState(
+    BackgroundVariant.Cross,
+  );
+
   drop(ref);
 
   const customNodes = useMemo(() => {
     return {message: MessageNodeTile};
+  }, []);
+
+  const nodeColor = useCallback((node: Node) => {
+    if (node.type === NodeTypes.message.type) {
+      return "#6ede87";
+    }
+
+    return "#6865A5";
   }, []);
 
   return (
@@ -39,7 +58,37 @@ function ContentArea() {
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
-      />
+      >
+        <MiniMap
+          nodeColor={nodeColor}
+          zoomable
+          pannable
+        />
+        <Background
+          color="#ccc"
+          variant={backgroundVariant}
+        />
+        <Panel position="top-left">
+          <button
+            className={`${styles["flow-panel"]} ${backgroundVariant === BackgroundVariant.Dots && styles.active}`}
+            onClick={() => setBackgroundVariant(BackgroundVariant.Dots)}
+          >
+            Dot
+          </button>
+          <button
+            className={`${styles["flow-panel"]} ${backgroundVariant === BackgroundVariant.Cross && styles.active}`}
+            onClick={() => setBackgroundVariant(BackgroundVariant.Cross)}
+          >
+            Cross
+          </button>
+          <button
+            className={`${styles["flow-panel"]} ${backgroundVariant === BackgroundVariant.Lines && styles.active}`}
+            onClick={() => setBackgroundVariant(BackgroundVariant.Lines)}
+          >
+            Lines
+          </button>
+        </Panel>
+      </ReactFlow>
     </div>
   );
 }
